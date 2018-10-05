d67346f12f6ee762f7d12d242832c99c980cc680
baguinaga <42448094+Baguinaga@users.noreply.github.com>
2018-10-05 19:43:28 -0400

    bootstrap prototype

---
+++ assets/javascript/script.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+  apiKey: "AIzaSyDs4XpeSdXlCZIfbJCZ6WfW-AFh4d3sMOM",
+  authDomain: "classactivity-9843d.firebaseapp.com",
+  databaseURL: "https://classactivity-9843d.firebaseio.com",
+  projectId: "classactivity-9843d",
+  storageBucket: "classactivity-9843d.appspot.com",
+  messagingSenderId: "903829561659"
+};
+firebase.initializeApp(config);
